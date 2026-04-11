1b1ceb12b8d0b688c5e4945e88bb2f255c162486
gabriel-pinotti <gabriel.apinotti06@gmail.com>
2026-04-11 02:48:57 -0300

    initial_commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def criar_mapa(linhas, colunas):
+    return [['—'] * colunas for _ in range(linhas)]
+
+def imprimir_mapa(mapa):
+    for linha in mapa:
+        print(' '.join(linha))
+
+def main():
+    print("=== Campo Minado ===")
+    linhas = int(input("Número de linhas: "))
+    colunas = int(input("Número de colunas: "))
+
+    mapa = criar_mapa(linhas, colunas)
+    imprimir_mapa(mapa)
+
+if __name__ == "__main__":
+    main()
